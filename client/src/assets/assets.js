52b947197be0b15838a873fe475d2a49584d34c3
nandinisharma03 <nandinisharma.3005@gmail.com>
2026-02-20 01:25:19 +0530

    Add magic icon to assets

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -18,6 +18,7 @@ import cross_icon from './cross_icon.svg'
 import star_group from './star_group.png'
 import credit_star from './credit_star.svg'
 import profile_icon from './profile_icon.png'
+import download_icon from './download_icon.svg'
 
 export const assets = {
     logo,
@@ -34,7 +35,9 @@ export const assets = {
     cross_icon,
     star_group,
     credit_star,
-    profile_icon
+    profile_icon,
+    download_icon,
+    magic_icon: download_icon
 }
 
 export const stepsData = [
